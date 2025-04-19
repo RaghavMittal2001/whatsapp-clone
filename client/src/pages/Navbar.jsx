27246fcdaefeb8996e0800fcec10e6e8a181c6c2
RaghavMittal2001 <raghavmittal1389@gmail.com>
2025-04-19 19:23:43 +0530

    error4

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import Logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
+
 function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-stale" style={{backgroundColor :"#d9f1d2"}}>
+            <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#d9f1d2"}}>
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">
                         <img src={Logo} alt="Company Logo" height={20} width={30} />
                     </Link>
-                    <nav className="flex space-x-4 sm:justify-center">
+                    <div className="flex space-x-4 sm:justify-center">
                         {[
                             ['Home', '/'],
                             ['Abouts', '/Abouts'],
                             ['Login', '/email'],
                             ['Register', '/register'],
                         ].map(([title, url], index) => (
-                            <a  key={index} href={url} className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">{title}</a>
+                            <Link key={index} to={url} className="px-3 py-2 font-medium rounded-lg text-slate-700 hover:bg-slate-100 hover:text-slate-900">
+                                {title}
+                            </Link>
                         ))}
-                    </nav>
+                    </div>
                 </div>
             </nav>
         </div>
