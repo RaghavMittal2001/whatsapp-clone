f9133c666a2ae320398b069833a507b04a3ba27d
RaghavMittal2001 <raghavmittal1389@gmail.com>
2025-04-19 18:47:30 +0530

    error2

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,19 +1,18 @@
-import express from 'express';
-import Checkemail from '../controller/Checkemail.js';
-import Checkpassword from '../controller/Checkpassword.js';
-import Userdetails from '../controller/Userdetails.js';
-import Logout from '../controller/Logout.js';
-import UpdateUser from '../controller/Updateuserdetails.js';
-import Searchuser from '../controller/searchuser.js';
-import Registeruser from '../controller/RegisterUser.js';
+import express from "express";
+const router = express.Router();
+import Registeruser from "../controller/RegisterUser.js"; //importing Registeruser controller
+import Checkemail from "../controller/Checkemail.js"; //importing Checkemail controller
+import Checkpassword from "../controller/Checkpassword.js"; //importing Checkpassword controller
+import Userdetails from "../controller/UserDetails.js"; //importing Userdetails controller
+import Logout from "../controller/Logout.js"; //importing Logout controller
+import UpdateUser from "../controller/Updateuserdetails.js"; //importing UpdateUser controller
+import Searchuser from "../controller/Searchuser.js"; //importing Searchuser controller
 
-const router= express.Router();
-
-router.post('/register',Registeruser);//create API
-router.post('/LoginEmail',Checkemail);//checking User email 
-router.post('/LoginPassword',Checkpassword) // Checking User password 
-router.get('/userDetails',Userdetails) //Getting user details
-router.get('/Logout',Logout) //Logout user 
-router.post('/updateuser',UpdateUser) //updating user details 
-router.post('/searchuser',Searchuser) //search  all users for one login user 
-export default router; 
+router.post("/register", Registeruser); //create API
+router.post("/LoginEmail", Checkemail); //checking User email
+router.post("/LoginPassword", Checkpassword); // Checking User password
+router.get("/userDetails", Userdetails); //Getting user details
+router.get("/Logout", Logout); //Logout user
+router.post("/updateuser", UpdateUser); //updating user details
+router.post("/searchuser", Searchuser); //search  all users for one login user
+export default router;
