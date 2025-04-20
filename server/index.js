bd909689331b17445f6050e28619a89f4bdff3ef
RaghavMittal2001 <raghavmittal1389@gmail.com>
2025-04-20 09:08:37 +0530

    iiii

--- server/index.js
+++ server/index.js
@@ -11,9 +11,19 @@ dotenv.config();
 // const app =express();
 connectDB();
 app.use(cors({
-    origin: 'https://whatsapp-clone-bay-three.vercel.app', // Allow this specific origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    credentials: true // Allow credentials if needed
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            "https://whatsapp-clone-bay-three.vercel.app",
+            "https://whatsappclone-orcin.vercel.app",
+        ];
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser())
